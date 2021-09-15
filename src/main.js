089d02c44816d3103d3c622f9d314e1f3c6f2798
elizabetabeta <66870794+elizabetabeta@users.noreply.github.com>
2021-09-15 07:31:21 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import Vuetify from 'vuetify/lib'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
